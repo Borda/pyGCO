b86fe3356f8b99e7f578d92f10c349eaac559bd4
Jirka <jirka.borovec@seznam.cz>
2023-06-13 01:52:52 +0200

    about

--- src/gco/__init__.py
+++ src/gco/__init__.py
@@ -2,14 +2,14 @@
 
 import numpy
 
-from .__about__ import *  # noqa: F403
-
 # patch for numpy 1.24+
 for name, tp in [("int", int), ("float", float), ("bool", bool)]:
     if not hasattr(numpy, name):
         setattr(numpy, name, tp)
 
 try:
+    from __about__ import *  # noqa: F403
     from pygco import *  # noqa: F401 F403
 except ImportError:
+    from gco.__about__ import *  # noqa: F403
     from gco.pygco import *  # noqa: F401 F403
